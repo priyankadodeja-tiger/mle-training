c8fe4bb28340724886757eb9e8f18aa9de044135
Priyanka Dodeja <priyanka.dodeja@tigeranalytics.com>
2023-06-23 11:06:22 +0530

    file fixed in VScode

---
+++ nonstandard_code.py
@@ -0,0 +1,12 @@
+# Function to demonstrate printing pattern
+def pypart(n):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print("* ", end="")
+
+        print("\r")
+
+
+# Driver Code
+n = 5
+pypart(n)
